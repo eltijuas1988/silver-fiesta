75b024836acd18e46a2cf58f7bedfa6b21b22682
Daniel Rojas <crimsondex@yahoo.com>
2018-12-27 14:54:55 -0800

    Imported axios and created sendRequest handler.

--- src/Body/InputBox.js
+++ src/Body/InputBox.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import TextField from '@material-ui/core/TextField'
 
 class InputBox extends React.PureComponent {
@@ -14,6 +15,24 @@ class InputBox extends React.PureComponent {
 
   onKeyDown = (e) => {
     if (e.key === "Enter") {console.log("Enter Clicked")}
+  }
+
+  sendRequest = () => {
+    let apiKey = 'b2077944e95bdb0c5e00862011d622ad'
+    let city = 'portland'
+    let url =
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    axios.get(`${url}`)
+    .then(res => {
+      const {data} = res
+      const main = data && data.main
+      const {temp} = main
+      console.log({temp})
+    })
+    .catch(error => {
+      console.log({error})
+    })
   }
 
   render() {
