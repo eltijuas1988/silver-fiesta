f3e2c697ad889384d448a1f02af66caadef561be
Daniel Rojas <crimsondex@yahoo.com>
2018-12-26 18:30:04 -0800

    Deleted unused files.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-// import logo from './logo.svg';
-import './App.css'
 import Header from './Header'
 import Body from './Body'
 import Footer from './Footer'
@@ -9,23 +7,9 @@ class App extends Component {
   render() {
     return (
       <div>
-          {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header> */}
-          <Header/>
-          <Body/>
-          <Footer/>
+        <Header/>
+        <Body/>
+        <Footer/>
       </div>
     );
   }
