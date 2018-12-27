0fcac58cfeb123bd73a3e94074f483f25d0088a4
Daniel Rojas <crimsondex@yahoo.com>
2018-12-26 18:37:56 -0800

    Cleaned up and added styles to top of tree.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+import injectSheet from 'react-jss'
 import Header from './Header'
 import Body from './Body'
 import Footer from './Footer'
 
 class App extends Component {
   render() {
+    const {classes} = this.props
+
     return (
-      <div>
+      <div className={classes.main}>
         <Header/>
         <Body/>
         <Footer/>
@@ -15,4 +18,13 @@ class App extends Component {
   }
 }
 
-export default App
+const styles = {
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 40,
+  },
+}
+
+export default injectSheet(styles)(App)
