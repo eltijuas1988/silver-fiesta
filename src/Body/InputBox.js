2a8d464057f83120f83a224482dfd439c061ea13
Daniel Rojas <crimsondex@yahoo.com>
2018-12-27 21:15:47 -0800

    Changed InputBox to functional component and removed unused functions.

--- src/Body/InputBox.js
+++ src/Body/InputBox.js
@@ -1,39 +1,26 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
 
-class InputBox extends React.PureComponent {
-  state = {
-    error: null,
-  }
-
-  onChange = (e) => {
-    this.props.onChange(e)
-  }
-
-  render() {
-    const {error} = this.state
-    const {city} = this.props
-
-    return (
-      <TextField
-        error={!!error}
-        id="outlined-full-width"
-        label={"City"}
-        style={{paddingBottom: error ? 0 : 20}}
-        helperText={error}
-        placeholder="Please Enter a Name For a City"
-        fullWidth
-        margin="none"
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        autoFocus
-        value={city}
-        onChange={this.onChange}
-      />
-    )
-  }
+const InputBox = ({city, error, onChange}) => {
+  return (
+    <TextField
+      error={!!error}
+      id="outlined-full-width"
+      label={"City"}
+      style={{paddingBottom: error ? 0 : 20}}
+      helperText={error}
+      placeholder="Please Enter a Name For a City"
+      fullWidth
+      margin="none"
+      variant="outlined"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      autoFocus
+      value={city}
+      onChange={onChange}
+    />
+  )
 }
 
 export default InputBox
