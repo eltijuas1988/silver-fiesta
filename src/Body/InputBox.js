a34863e4ae00032e5c5beb9c54d774b392276f22
Daniel Rojas <crimsondex@yahoo.com>
2018-12-27 18:51:53 -0800

    When Enter is pressed, request is sent and temperature is displayed.

--- src/Body/InputBox.js
+++ src/Body/InputBox.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import axios from 'axios'
 import TextField from '@material-ui/core/TextField'
-
-const domain = "http://api.openweathermap.org/data/2.5/weather"
-const apiKey = 'b2077944e95bdb0c5e00862011d622ad'
 
 class InputBox extends React.PureComponent {
   state = {
@@ -12,34 +8,6 @@ class InputBox extends React.PureComponent {
 
   onChange = (e) => {
     this.props.onChange(e)
-  }
-
-  onKeyDown = (e) => {
-    if (e.key === "Enter") {this.sendRequest()}
-  }
-
-  sendRequest = () => {
-    const {city} = this.props
-
-    if (!city || !city.length > 0) return
-    const params = `?q=${city}&units=imperial&appid=${apiKey}`
-    let url = `${domain}${params}`
-
-    axios.get(`${url}`)
-    .then(res => {
-      this.setState({error: null})
-
-      const {data} = res
-      const main = data && data.main
-      const {temp} = main
-
-      console.log({temp})
-    })
-    .catch(error => {
-      this.setState({error: "Please Enter a Valid City Name"})
-
-      console.log({error})
-    })
   }
 
   render() {
@@ -63,7 +31,6 @@ class InputBox extends React.PureComponent {
         autoFocus
         value={city}
         onChange={this.onChange}
-        onKeyDown={this.onKeyDown}
       />
     )
   }
--- src/Body/TemperatureDisplay.js
+++ src/Body/TemperatureDisplay.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import axios from 'axios'
 import injectSheet from 'react-jss'
 import InputBox from './InputBox'
 import Message from './Message'
@@ -6,6 +7,8 @@ import Message from './Message'
 class TemperatureDisplay extends React.PureComponent {
   state = {
     city: "",
+    temperature: undefined,
+    error: undefined,
   }
 
   onChange = (e) => {
@@ -14,16 +17,61 @@ class TemperatureDisplay extends React.PureComponent {
     })
   }
 
+  onKeyDown = (e) => {
+    if (e.key === "Enter") {this.sendRequest()}
+  }
+
+  updateTemperature = ({temp}) => {
+    this.setState({temperature: temp})
+  }
+
+  updateError = ({error}) => {
+    this.setState({error})
+  }
+
+  sendRequest = () => {
+    const domain = "http://api.openweathermap.org/data/2.5/weather"
+    const apiKey = 'b2077944e95bdb0c5e00862011d622ad'
+
+    const {city} = this.state
+
+    if (!city || !city.length > 0) return null
+    const params = `?q=${city}&units=imperial&appid=${apiKey}`
+    let url = `${domain}${params}`
+
+    axios.get(`${url}`)
+    .then(res => {
+      const {data} = res
+      const main = data && data.main
+      const {temp} = main
+
+      this.updateTemperature({temp})
+
+      console.log({temp})
+    })
+    .catch(error => {
+      this.updateError({error})
+      console.log({error})
+    })
+  }
+
+  composeTemperatureMessage = () => {
+    const {city, temperature} = this.state
+    if (!city && !temperature) return null
+
+    return `The current temperature in ${city} is ${temperature} degrees F.`
+  }
+
   render() {
     const {classes} = this.props
     const {city} = this.state
 
     return (
-      <div>
+      <div onKeyDown={this.onKeyDown}>
         <div className={classes.input}>
           <InputBox city={city} onChange={this.onChange}/>
         </div>
-        <Message/>
+        <Message message={this.composeTemperatureMessage()}/>
       </div>
     )
   }
