871aabcb580d990bf285140fc00515a12cf12c5b
Daniel Rojas <crimsondex@yahoo.com>
2019-02-03 23:41:21 -0800

    Deleted index.css and moved styles to App.js.

--- src/App.js
+++ src/App.js
@@ -32,10 +32,37 @@ const styles = {
     },
     body: {
       height: '100%',
+      margin: 0,
+      padding: 0,
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+        'sans-serif'
+      ],
+      '-webkit-font-smoothing': 'antialiased',
+      '-moz-osx-font-smoothing': 'grayscale',
     },
     '#root': {
       height: '100%',
     },
+    code: {
+      fontFamily: [
+        'source-code-pro',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Courier New',
+        'monospace'
+      ],
+    }
   }
 }
 
